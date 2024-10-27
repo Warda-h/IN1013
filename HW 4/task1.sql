abe8f433b9f35e51a6b9cf406520c5749639ce6f
Warda-h <warda.hussein@city.ac.uk>
2024-10-27 20:26:36 +0000

    Update task1.sql

--- HW 4/task1.sql
+++ HW 4/task1.sql
@@ -4,18 +4,18 @@ USE pet_database;
 DROP TABLE IF EXISTS petPet, petEvent;
 CREATE TABLE petPet(
 petname VARCHAR(20),
-owner VARCHAR(20),
-species VARCHAR(20),
+owner VARCHAR(45),
+species VARCHAR(45),
 gender ENUM('M','F'),
-birth  INTEGER(6),
-death  INTEGER(6)
+birth  DATE,
+death  DATE
 );
 ALTER TABLE petPet ADD PRIMARY KEY(petname,owner);
 CREATE TABLE petEvent(
 petname VARCHAR(20),
-eventdate INTEGER(6),
-eventtype VARCHAR(20),
-remark VARCHAR(40),
+eventdate DATE,
+eventtype VARCHAR(15),
+remark VARCHAR(255),
 FOREIGN KEY (petname) REFERENCES petPet(petname),
 PRIMARY KEY (petname,eventdate)
 );
